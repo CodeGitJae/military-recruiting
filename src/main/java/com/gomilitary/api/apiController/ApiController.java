f456692bb020a6d8ebb4e03ec7336d61b429f11d
CodeGitJae <CodeGitJae@gmail.com>
2024-09-24 22:38:54 +0900

    컨트롤러 코드 수정

--- src/main/java/com/gomilitary/api/apiController/ApiController.java
+++ src/main/java/com/gomilitary/api/apiController/ApiController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gomilitary.api.entity.teukgiCodeEntity;
 import com.gomilitary.api.service.teukgiCodeApiService;
 import lombok.RequiredArgsConstructor;
@@ -15,20 +14,11 @@ public class ApiController {
   private final teukgiCodeApiService teukgiCodeApiService;
 
     @GetMapping("/get-data")
-    public ResponseEntity<String> helloWorld() {
+    public ResponseEntity<List<teukgiCodeEntity>> helloWorld() {
 
       List<teukgiCodeEntity> dataArr = teukgiCodeApiService.getSavedData();
-    //  System.out.println("dataArr::::::::::::::::::::::::"+dataArr);
-      ObjectMapper objectMapper = new ObjectMapper();
-
-      try {
-        String jsonString = objectMapper.writeValueAsString(dataArr);
-//        System.out.println("jsonString:::::::::::::::::::::::"+jsonString);
-          return new ResponseEntity<>(jsonString, HttpStatus.OK);
-      } catch (Exception e) {
-        e.printStackTrace();
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-      }
-
+      System.out.println("dataArr::::::::::::::::::::::::"+dataArr);
+      
+      return new ResponseEntity<List<teukgiCodeEntity>>(dataArr, HttpStatus.OK); 
     }
 }
